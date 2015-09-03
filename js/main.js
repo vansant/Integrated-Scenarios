8947bde8465e91115d2fa00bbb08be69c431b55c
vansant <donny.vansant@gmail.com>
2015-09-03 10:07:43 -0700

    Added toggle on click to nav-bar hamburger so it closes when a page is selected

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 // jQuery Code
-$( document ).ready(function() {
-    $( "#body-content" ).load( "pages/home.html", function() {
-    });
-    $("#nav-items li").click(function() {
-      // Nav item name
-      var nav_item = $(this)
+$(document).ready(function() {
+    // Load home page initially
+    $("#body-content").load("pages/home.html", function() {});
 
-      // Use nav item name to chose which page to load
-      var nav_item_name = $(this).text().toLowerCase().trim();
-      var page_to_load = nav_item_name + ".html";
-      console.log(page_to_load);
-      $( "#body-content" ).load( "pages/" + page_to_load, function() {
-      });
-      
-      // Clear active nav item
-      $("#nav-items li").removeClass("active")
-      // Set nav item as active
-      nav_item.addClass("active")
+    // On click of nav-bar items
+    $("#nav-items li").click(function() {
+        // Nav item name
+        var nav_item = $(this)
+
+        // Use nav item name to chose which page to load
+        var nav_item_name = $(this).text().toLowerCase().trim();
+        var page_to_load = nav_item_name + ".html";
+        console.log(page_to_load);
+        $("#body-content").load("pages/" + page_to_load, function() {});
+
+        // Clear active nav item
+        $("#nav-items li").removeClass("active")
+            // Set nav item as active
+        nav_item.addClass("active")
+
+        // If nav-bar items are collapsed - small screens
+        if ($(window).width() < 768) {
+            // Close Hamburger
+            try {
+                $(".navbar-toggle").click()
+            } catch (error) {
+                console.log("error: ", error.message);
+            }
+        }
     });
 });
